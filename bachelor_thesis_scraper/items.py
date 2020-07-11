a5450279f8700e698bf590f724f2539d10219327
Manh Tin <mtnguyen47@gmail.com>
2020-07-11 13:16:20 +0200

    add webcam item

--- bachelor_thesis_scraper/items.py
+++ bachelor_thesis_scraper/items.py
@@ -5,8 +5,12 @@
 
 import scrapy
 
-
 class BachelorThesisScraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class WebcamImageItem(scrapy.Item):
+    name = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
